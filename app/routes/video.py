b78a9aaf1bb682f189737cf1e6dc835a07e670ea
enzomastra <mastraenzo@gmail.com>
2025-05-06 13:55:11 -0300

    changes

--- app/routes/video.py
+++ app/routes/video.py
@@ -1,26 +1,23 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException
 from app.services.video_service import process_video
 from ..utils.file_utils import is_allowed_file
-import shutil
 import os
 
 router = APIRouter()
-
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @router.post("/analyze")
 async def analyze_video(file: UploadFile = File(...)):
     if not is_allowed_file(file.filename):
         raise HTTPException(status_code=400, detail="Formato de archivo no compatible. Solo .mp4 es permitido.")
 
-    video_path = os.path.join(UPLOAD_DIR, file.filename)
-
-    with open(video_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
     try:
-        result = process_video(video_path)
+        temp_file_path = f"./uploads/{file.filename}"
+        with open(temp_file_path, "wb") as buffer:
+            buffer.write(await file.read())
+
+        result = process_video(temp_file_path)
+        
+        os.remove(temp_file_path)
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al procesar el video: {str(e)}")
